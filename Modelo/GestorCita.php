f4da34ef54ad06d12f8f779971d086ed53549f2e
Leidi11 <leidyflorez2002@gmail.com>
2025-05-12 15:18:09 -0500

    Initial commit

---
+++ Modelo/GestorCita.php
@@ -0,0 +1,20 @@
+Este código define una clase Paciente en PHP que se utiliza para manejar los datos relacionados con un paciente, como su identificación, nombres, apellidos, fecha de nacimiento y sexo.
+
+Explicación:
+Propiedades privadas:
+
+$identificacion: Almacena el número de identificación del paciente.
+$nombres: Almacena el nombre del paciente.
+$apellidos: Almacena el apellido del paciente.
+$fechaNacimiento: Almacena la fecha de nacimiento del paciente.
+$sexo: Almacena el sexo del paciente.
+Método constructor: El constructor __construct inicializa las propiedades con los valores que se pasan como parámetros cuando se crea un objeto de la clase Paciente.
+
+Métodos:
+
+obtenerIdentificacion(): Retorna el número de identificación del paciente.
+obtenerNombres(): Retorna el nombre del paciente.
+obtenerApellidos(): Retorna el apellido del paciente.
+obtenerFechaNacimiento(): Retorna la fecha de nacimiento del paciente.
+obtenerSexo(): Retorna el sexo del paciente.
+Este diseño permite encapsular la información de un paciente y proporcionarle métodos para acceder a esos datos de manera estructurada.
